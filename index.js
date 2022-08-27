54b961116f4f7359e94350bd434fd3427a5663f7
Margaret Kenda <margaret.kenda@yahoo.com>
2022-08-27 11:39:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+   we shall iterate from the beginning of the string to the middle,  we then compare the letter we are iterating over to the corresponding letter at the end of the string
+    if the letters don't match,  it returns false
+if we reach the middle, and all the letters match, it returns true
 */
 
 /*
